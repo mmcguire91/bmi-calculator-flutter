fe9ef8c92282c84e566787eca91482edb562d5e4
mmcguire91 <mmcguire91@gmail.com>
2020-05-28 16:19:11 -0500

    Initial commit

---
+++ lib/input_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFFea1556),
+        foregroundColor: Colors.white,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
