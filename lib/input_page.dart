a776f3b359646e6be79fd0ab01bfb079e06994e4
mmcguire91 <mmcguire91@gmail.com>
2020-05-30 08:29:48 -0500

    Built bottom bar, refactored color using constant

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0; //must include decimal .0
+const activeCardColor = Color(0xFF1d1f33); //color of active cards
+const bottomBarColor = Color(0xFFea1556); //color of bottom bar
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -22,12 +26,12 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                   child: ReusableContainer(
-                    cardColor: Color(0xFF1d1f33),
+                    cardColor: activeCardColor,
                   ), //refactored container
                 ),
                 Expanded(
                   child: ReusableContainer(
-                    cardColor: Color(0xFF1d1f33),
+                    cardColor: activeCardColor,
                   ), //refactored container
                 ),
               ],
@@ -38,7 +42,7 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                   child: ReusableContainer(
-                    cardColor: Color(0xFF1d1f33),
+                    cardColor: activeCardColor,
                   ), //refactored container
                 ),
               ],
@@ -49,19 +53,32 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                   child: ReusableContainer(
-                    cardColor: Color(0xFF1d1f33),
+                    cardColor: activeCardColor,
                   ), //refactored container
                 ),
                 Expanded(
                   child: ReusableContainer(
-                    cardColor: Color(0xFF1d1f33),
+                    cardColor: activeCardColor,
                   ), //refactored container
                 ),
               ],
             ),
           ),
+          BottomBar()
         ],
       ),
+    );
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: bottomBarColor,
+      margin: EdgeInsets.only(top: 10),
+      width: double.infinity, //width equal to device size
+      height: bottomContainerHeight,
     );
   }
 }
@@ -70,7 +87,7 @@ class ReusableContainer extends StatelessWidget {
   ReusableContainer({@required this.cardColor});
 
   final Color
-      cardColor; // establishing a dynamic color to the Reusable Container
+      cardColor; // establishing a color property to the Reusable Container
 
   @override
   Widget build(BuildContext context) {
